18ba16176b15c232696269063148970076a03514
Muhammad Aryan <160228293+MuhammadAriyan@users.noreply.github.com>
2025-08-24 16:56:54 +0500

    added download option and improved ui for the button

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -93,7 +93,10 @@ export default function HomePage() {
           </CardSwap>
         </div>
       </div>
-      <div className="absolute bg-blue-500 border-2 bottom-10 left-5 md:left-10 z-20 max-w-[90vw] sm:max-w-md">
+      <a 
+      href="/resume.pdf" 
+      download 
+        className="absolute bg-blue-500 px-4 py-2 text-white border-2 bottom-10 left-5 md:left-10 z-20 max-w-[90vw] sm:max-w-md">
         View My Resume ❄
         </div>
     </main>
